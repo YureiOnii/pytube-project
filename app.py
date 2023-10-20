11a48e41ed73bfc300da41ac7b55f79ea78b1f94
YureiOnii <bakaryanoni@hotmail.com>
2023-10-20 15:50:14 -0300

    deseja fazer download

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 from pytube import YouTube
+from os import system as cmd
+from time import sleep as esperar
 
-yt = YouTube(input("Digite a ulr do video que deseja baixar: "))
+while True:
+    try:
+        print(100 * "=")
+        print("Bem-vindo ao instalador de video")
+        print(100 * "=")
+        esperar(1)
+        cmd ("cls")
+        
+        yt = YouTube(input("Digite a url que deseja fazer o download: "))
+        print(100 * "=")
+        esperar(1)
 
-print(yt.title)
-desc = yt.description
-if yt.description:
-    print(desc)
+        print(yt.title)
+        desc = yt.description
 
-else:
-    print("Descrição indisponível")
+        if yt.description:
+            print(desc)
+
+        else:
+            print("Descrição inválida!!")
+        
+        print(100 * "=")
+        escolha = input("1-[Continuar]\n2-[Sair]\n")
+        print(100 * "=")
+
+        if escolha == "1":
+            input("Deseja fazer o download do video?")
+
+        elif escolha == "2":
+            break
+        
+        else:
+            print("Digite uma opção válida!!")
+
+    except KeyboardInterrupt:
+        print("Digite um comando válido!")
+        esperar(1)
+        cmd("cls")
+        break
