364bf8570274b49817648ef9f5640bb90a07cc7d
YureiOnii <bakaryanoni@hotmail.com>
2023-10-20 14:54:12 -0300

    tratamento de desc

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from pytube import YouTube
 
 yt = YouTube(input("Digite a ulr do video que deseja baixar: "))
+
+print(yt.title)
+desc = yt.description
+if yt.description:
+    print(desc)
+
+else:
+    print("Descrição indisponível")
