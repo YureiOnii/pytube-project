2f184ece1368b68a16504618af2d351a0623c276
YureiOnii <bakaryanoni@hotmail.com>
2023-10-25 13:48:36 -0300

    Finalizado

--- app.py
+++ app.py
@@ -1,54 +1,68 @@
-from pytube import YouTube
-from os import system as cmd
-from time import sleep as esperar
+from pytube import YouTube, Playlist
 
-while True:
+def get_video_info(video_url):
     try:
-        print(100 * "=")
-        print("Bem-vindo ao instalador de video")
-        print(100 * "=")
-        esperar(1)
-        cmd ("cls")
-        
-        yt = YouTube(input("Digite a url que deseja fazer o download: "))
-        print(100 * "=")
-        esperar(1)
+        yt = YouTube(video_url)
+        title = yt.title
+        author = yt.author
+        length = yt.length
+        return {
+            "title": title,
+            "author": author,
+            "length": length
+        }
+    except Exception as e:
+        return {"error": str(e)}
 
-        video = yt
+def download_video(video_url, output_path=".\midias"):
+    try:
+        yt = YouTube(video_url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download(output_path)
+        return f"Downloaded: {yt.title}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+ 
+def get_pvideo_info(video_url):
+    try:
+        yt = YouTube(video_url)
+        title = yt.title
+        author = yt.author
+        length = yt.length
+        return {
+            "title": title,
+            "author": author,
+            "length": length
+        }
+    except Exception as e:
+        return {"error": str(e)}   
 
-        print(yt.title)
-        desc = yt.description
+def download_playlist(playlist_url, output_path=".\midias"):
+    try:
+        playlist = Playlist(playlist_url)
+        for video_url in playlist.video_urls:
+            download_video(video_url, output_path)
+        return f"Downloaded {len(playlist.video_urls)} videos from the playlist: {playlist.title}"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-        if yt.description:
-            print(desc)
+# Exemplo de uso:
+video_url = input("Digite o link do vídeo: ")
+playlist_url = input("Digite o link da playlst: ")
 
-        else:
-            print("Descrição inválida!!")
-        
-        print(100 * "=")
-        escolha = input("1-[Continuar]\n2-[Sair]\n")
-        print(100 * "=")
-        esperar(1.5)
-        cmd("cls")
+# Obtém informações do vídeo
+video_info = get_video_info(video_url)
+print("Informações do vídeo:")
+print(video_info)
 
-        if escolha == "1":
-            escolha2 = input("Deseja fazer o download do video?\n[s]-Sim\n[n]-Não\n")
+# executa para baixar um vídeo
+download_result = download_video(video_url)
+print(download_result)
 
-            if escolha2 == "s":
-                print(yt.title)
-                print(desc)
-
-            elif escolha2 == "n":
-                continue
-
-        elif escolha == "2":
-            break
-
-        else:
-            print("Digite uma opção válida!!")
-
-    except KeyboardInterrupt:
-        print("Digite um comando válido!")
-        esperar(1)
-        cmd("cls")
-        break
+# executa para baixar uma playlist
+pvideo = get_pvideo_info(video_url)
+playlist_download_result = download_playlist(playlist_url)
+print(playlist_download_result)
+print("Informações da playlist:")
+print(video_info)
